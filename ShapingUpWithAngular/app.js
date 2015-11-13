94eb82698564be365a67143747c6b7e6967096e8
Steve Bedell <bedell.369@osu.edu>
2015-11-13 17:03:50 -0500

    updating thru level 3

--- ShapingUpWithAngular/app.js
+++ ShapingUpWithAngular/app.js
@@ -7,12 +7,37 @@ app.controller('StoreController', function() {
 app.controller("PanelController", function() {
     this.tab = 1;
 
-    this.selectTab = function(setTab) {
-        this.tab = setTab;
+    this.selectTab = function(newValue) {
+        this.tab = newValue;
     };
 
     this.isSelected = function(checkTab) {
         return this.tab === checkTab;
+    };
+});
+
+app.controller('GalleryController', function() {
+    this.current = 0;
+
+    this.setCurrent = function(newGallery) {
+        this.current = newGallery || 0;
+    };
+});
+
+app.controller("ReviewController", function() {
+    this.review = {};
+
+    this.addReview = function(product){
+        this.review.createdOn = Date.now();
+        product.reviews.push(this.review);
+        this.review = {};
+    };
+});
+
+app.directive("productDescription", function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'product-description.html'
     };
 });
 
@@ -26,12 +51,8 @@ var gems = [
         soldOut: false,
         image: '../img/skytree1.png',
         images: [
-            {
-                full: '../img/skytree1.png'
-            },
-            {
-                full: '../img/skytree2.jpg'
-            }
+            '../img/skytree1.png',
+            '../img/skytree2.jpg'
         ],
         reviews: [
             {
