99156138d958636bab6d521f3f300834fb5bf5c5
Steve Bedell <bedell.369@osu.edu>
2015-11-20 14:58:33 -0500

    adding new files

---
+++ ShapingUpWithAngular/productDirectives.js
@@ -0,0 +1,42 @@
+(function() {
+    var app = angular.module('product-directives', []);
+
+    app.directive("productDescription", function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-description.html'
+        };
+    });
+
+    app.directive("productReviews", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "product-reviews.html"
+        };
+    });
+
+    app.directive("productSpecs", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "product-specs.html"
+        };
+    });
+
+    app.directive("productTabs", function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-tabs.html',
+            controller: function() {
+                this.tab = 1;
+                this.isSet = function(checkTab) {
+                    return this.tab === checkTab;
+                };
+
+                this.setTab = function(setTab) {
+                    this.tab = setTab;
+                };
+            },
+            controllerAs: "tab"
+        };
+    });
+})();
