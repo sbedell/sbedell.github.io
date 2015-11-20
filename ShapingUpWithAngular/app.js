2b61356d1a9f2f982fedf6e98a1806e79fc0cea4
Steve Bedell <bedell.369@osu.edu>
2015-11-20 14:45:01 -0500

    finishing up shaping up with angular

--- ShapingUpWithAngular/app.js
+++ ShapingUpWithAngular/app.js
@@ -1,117 +1,91 @@
-var app = angular.module('store', []);
+(function() {
+    // array is all the depdencies
+    var app = angular.module('store', ['product-directives']);
 
-app.controller('StoreController', function() {
-    this.products = gems;
-});
+    app.controller('StoreController', [ '$http', function($http) {
+        this.products = gems;
+        // var store = this;
+        // store.products = [];
 
-app.controller("PanelController", function() {
-    this.tab = 1;
+        // $http.get('store-products.json', {"Content-Type": "application/json"}).then(function(gems) {
+        //     store.products = gems;
+        // });
+        // $http({method: "GET", url: "store-products.json", data: "", headers: {"Content-Type": "application/json"}})
+        // .then(function(gems) {
+        //     store.products = gems;
+        // })
+    }]);
 
-    this.selectTab = function(newValue) {
-        this.tab = newValue;
-    };
+    app.controller("PanelController", function() {
+        this.tab = 1;
 
-    this.isSelected = function(checkTab) {
-        return this.tab === checkTab;
-    };
-});
+        this.selectTab = function(newValue) {
+            this.tab = newValue;
+        };
 
-app.controller('GalleryController', function() {
-    this.current = 0;
+        this.isSelected = function(checkTab) {
+            return this.tab === checkTab;
+        };
+    });
 
-    this.setCurrent = function(imageNumber) {
-        this.current = imageNumber || 0;
-    };
-});
+    app.controller('GalleryController', function() {
+        this.current = 0;
 
-app.controller("ReviewController", function() {
-    this.review = {};
+        this.setCurrent = function(imageNumber) {
+            this.current = imageNumber || 0;
+        };
+    });
 
-    this.addReview = function(product){
-        this.review.createdOn = Date.now();
-        product.reviews.push(this.review);
+    app.controller("ReviewController", function() {
         this.review = {};
-    };
-});
 
-app.directive("productDescription", function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'product-description.html'
-    };
-});
+        this.addReview = function(product){
+            this.review.createdOn = Date.now();
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
 
-app.directive("productReviews", function() {
-    return {
-        restrict: 'E',
-        templateUrl: "product-reviews.html"
-    };
-});
-
-app.directive("productSpecs", function() {
-    return {
-        restrict: 'E',
-        templateUrl: "product-specs.html"
-    };
-});
-
-app.directive("productTabs", function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'product-tabs.html',
-        controller: function() {
-            this.tab = 1;
-            this.isSet = function(checkTab) {
-                return this.tab === checkTab;
-            };
-
-            this.setTab = function(setTab) {
-                this.tab = setTab;
-            };
+    var gems = [
+        {
+            name: "Dodecahedron",
+            price: 2.95,
+            shine: 4,
+            rarity: 7,
+            faces: 14,
+            color: "#CCC",
+            description: "12 sides I think?",
+            canPurchase: false,
+            soldOut: false,
+            image: '../img/skytree1.png',
+            images: [
+                '../img/skytree1.png',
+                '../img/skytree2.jpg'
+            ],
+            reviews: [
+                {
+                    stars: 5,
+                    body: "I love this gem!",
+                    author: "john@cena.com"
+                },
+                {
+                    stars: 1,
+                    body: "This rock suxxx!",
+                    author: "tim@haterade.com"
+                }
+            ]
         },
-        controllerAs: "tab"
-    };
-});
-
-var gems = [
-    {
-        name: "Dodecahedron",
-        price: 2.95,
-        shine: 4,
-        rarity: 7,
-        faces: 14,
-        color: "#CCC",
-        description: "12 sides I think?",
-        canPurchase: false,
-        soldOut: false,
-        image: '../img/skytree1.png',
-        images: [
-            '../img/skytree1.png',
-            '../img/skytree2.jpg'
-        ],
-        reviews: [
-            {
-                stars: 5,
-                body: "I love this gem!",
-                author: "john@cena.com"
-            },
-            {
-                stars: 1,
-                body: "This rock suxxx!",
-                author: "tim@haterade.com"
-            }
-        ]
-    },
-    {
-        name: "Pentagonal Gem",
-        price: 5.95,
-        shine: 5,
-        rarity: 6,
-        faces: 5,
-        color: "#EEE",
-        description: "5 sided gem",
-        canPurchase: true,
-        soldOut: false,
-        image: '../img/skytree2.jpg'
-    },
-];
+        {
+            name: "Pentagonal Gem",
+            price: 5.95,
+            shine: 5,
+            rarity: 6,
+            faces: 5,
+            color: "#EEE",
+            description: "5 sided gem",
+            canPurchase: true,
+            soldOut: false,
+            image: '../img/skytree2.jpg'
+        },
+    ];
+})();
