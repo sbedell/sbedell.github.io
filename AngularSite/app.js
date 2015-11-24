401a8240b6cdb5f9ed764155606dd4d2ffe197e2
Steve Bedell <bedell.369@osu.edu>
2015-11-24 17:56:46 -0500

    More angular work

--- AngularSite/app.js
+++ AngularSite/app.js
@@ -1,7 +1,52 @@
 (function() {
     angular.module("myWebsite", [])
 
-    .controller("indexController", function() {
-        // code goes here
+    .controller("tabController", function() {
+        this.tab = 1;
+        this.isSet = function(checkTab) {
+            return this.tab === checkTab;
+        };
+
+        this.setTab = function(newTab) {
+            this.tab = newTab;
+        };
+    })
+
+    .controller("securityController", function() {
+        // rip code into here
+    })
+
+    .controller("storageController", function() {
+        // rip code into here
+    })
+
+    .directive("mainSection", function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/main-section.html',
+            controller: 'tabController',
+            controllerAs: 'tab'
+        };
+    })
+
+    .directive("securityPage", function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/security.html'
+        }
+    })
+
+    .directive("storageDemo", function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/storageDemo.html'
+        };
+    })
+
+    .directive("resumePage", function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/resume.html'
+        };
     });
 }());
