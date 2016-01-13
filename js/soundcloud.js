6dbfa6e768f2e7b50fa078b6047e16de9e3ed4e6
Steve Bedell <bedell.369@osu.edu>
2016-01-13 11:16:50 -0500

    more soundcloud page edits

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -3,7 +3,6 @@ SC.initialize({client_id:'75579df892fafb681bb657af3fe524b6'});
 $(document).ready(function() {
     SC.get('/tracks', { genres: 'dubstep' }, function(tracks) {
         $(tracks).each(function(index, track) {
-            //$('#results').append($('<li></li>').html('<a target=\'blank\' href=\'' + track.permalink_url +'\'>' + track.title + '</a>: ' + track.playback_count));
             $('#results').append($('<li></li>').html(`<a target="blank" href="${track.permalink_url}">${track.title}</a>: ${track.playback_count.toLocaleString()}`));
         });
     });
@@ -14,10 +13,7 @@ function setGenre() {
 	document.getElementById('results').innerHTML = '';
 	SC.get('/tracks', { genres: genre }, function(tracks) {
 		$(tracks).each(function(index, track) {
-			// $('#results').append($('<li></li>').html('<a target=\'blank\' href=\'' + track.permalink_url +'\'>' + track.title + '</a>: ' + track.playback_count));
-            $('#results').append($('<li></li>').html(`<a target=\'blank\' href=\'${track.permalink_url}\'>${track.title}</a>: ${track.playback_count.toLocaleString()}`));
+			$('#results').append($('<li></li>').html(`<a target="blank" href="${track.permalink_url}">${track.title}</a>: ${track.playback_count.toLocaleString()}`));
 		});
 	});
 }
-
-// $('#results').append($('<li></li>').html(track.title + ' - ' + track.playback_count));
