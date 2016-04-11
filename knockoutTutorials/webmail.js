0cc1591dc3b703858ea58ad0cc3c754ff37cebfd
Steve B <bedell.369@osu.edu>
2016-04-10 22:26:57 -0400

    adding email pages

---
+++ knockoutTutorials/webmail.js
@@ -0,0 +1,30 @@
+class WebmailViewModel {
+    constructor() {
+        let self = this;
+        
+        // Data
+        this.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
+        this.chosenFolderId = ko.observable();
+        this.chosenFolderData = ko.observable();
+        this.chosenMailData = ko.observable();
+
+        // Functions
+
+        this.goToFolder = function(folder) {
+            self.chosenFolderId(folder);
+            self.chosenMailData(null); // Stop showing an email
+            $.get('/mail', { folder: folder }, self.chosenFolderData);
+        }
+
+        this.goToMail = function(mail) {
+            self.chosenFolderId(mail.folder);
+            self.chosenFolderData(null); // Stop showing a folder
+            $.get("/mail", { mailId: mail.id }, self.chosenMailData);
+        };
+
+        // Show inbox by default
+        this.goToFolder('Inbox');
+    }
+};
+
+ko.applyBindings(new WebmailViewModel());
