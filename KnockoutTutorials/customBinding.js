8130a46533bca3d669e505f659d74ad1e2a52b9d
Steve Bedell <bedell.369@osu.edu>
2016-04-22 13:33:29 -0400

    updating knockout custom bindings page, removing some jquery ui

--- KnockoutTutorials/customBinding.js
+++ KnockoutTutorials/customBinding.js
@@ -1,48 +1,10 @@
-/*ko.bindingHandlers.starRating = {
-    init: function(element, valueAccessor) {
-        $(element).addClass("starRating");
-        for (let i = 0; i < 5; i++) {
-           $("<span>").appendTo(element);
-        }
-
-        // Handle mouse events on the stars
-        $("span", element).each(function(index) {
-            $(this).hover(
-                function() { $(this).prevAll().add(this).addClass("hoverChosen") },
-                function() { $(this).prevAll().add(this).removeClass("hoverChosen") }
-            ).click(() => {
-               let observable = valueAccessor();  // Get the associated observable
-               observable(index + 1);             // Write the new rating to it
-            });
-        });
-    },
-    update: function(element, valueAccessor) {
-        // Give the first x stars the "chosen" class, where x <= rating
-        let observable = valueAccessor();
-        $("span", element).each(function(index) {
-            $(this).toggleClass("chosen", index < observable());
-        });
-    }
-};*/
-
-ko.bindingHandlers.jqButton = {
-    init: (element) => {
-       $(element).button(); // Turns the element into a jQuery UI button
-    },
-    update: function(element, valueAccessor) {
-        let currentValue = valueAccessor();
-        // Here we just update the "disabled" state, but you could update other properties too
-        $(element).button("option", "disabled", currentValue.enable === false);
-    }
-};
-
 ko.bindingHandlers.fadeVisible = {
     init: (element, valueAccessor) => {
         // Start visible/invisible according to initial value
         let shouldDisplay = valueAccessor();
         $(element).toggle(shouldDisplay);
     },
-    update: function(element, valueAccessor) {
+    update: (element, valueAccessor) => {
         // On update, fade in/out
         let shouldDisplay = valueAccessor();
         shouldDisplay ? $(element).fadeIn() : $(element).fadeOut();
@@ -57,10 +19,12 @@ function Answer(text) {
 function SurveyViewModel(question, pointsBudget, answers) {
     this.question = question;
     this.pointsBudget = pointsBudget;
-    this.answers = $.map(answers, function(text) {
+    
+    this.answers = $.map(answers, (text) => {
         return new Answer(text);
     });
-    this.save = function() {
+    
+    this.save = () => {
         alert('To do');
     };
 
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ sbedell.github.io
 =================
 
 My Personal Site hosted via Github Pages
+
+***
+### Knockout Tutorials
+- Taken from http://learn.knockoutjs.com
