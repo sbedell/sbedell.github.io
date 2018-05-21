9d2510d0f6d396fd5382d8a6ba07ea625a7a588f
Steve B <bedell.369@osu.edu>
2018-05-21 10:20:08 -0400

    Deleting the Soundboard PWA directory and all files because I messed up caching and need to start fresh

--- soundboard/index.js
+++
@@ -1,7 +0,0 @@
-// Register service worker to control making site work offline
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/soundboard/serviceWorker.js').then(function() { 
-        console.log('Service Worker Registered'); 
-    });
-}
