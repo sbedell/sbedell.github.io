0220cb94998236d8abbd33ec46d55d307d8b3e77
Steve B <bedell.369@osu.edu>
2018-05-26 18:43:18 -0400

    Updating leafletMap.js

--- js/leafletMap.js
+++ js/leafletMap.js
@@ -1,20 +1,24 @@
+/**
+ * Leaflet and OpenStreetMap demo
+ */
+
 var mymap = L.map('myMap').setView([41.49985, -81.6938], 11);
-mymap.locate({setView: true, maxZoom: 16});
+
+mymap.locate({setView: true, maxZoom: 16}); // Locate user with Geolocation API
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors. BTW Fuck Google Maps!',
+    attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a>',
     maxZoom: 18
 }).addTo(mymap);
 
+// Addings some markers to the map, some with popups 
 var clevelandMarker = L.marker([41.49985, -81.6938]).addTo(mymap);
+
 var testMarker = L.marker([41.219857, -81.698456]).addTo(mymap);
 testMarker.bindPopup("<b>Whipps Ledges</b><br>Cleveland Metroparks", {'autoClose': false, closeOnClick: false}).openPopup();
 
 var testMarker2 = L.marker([41.318818, -81.617775]).addTo(mymap);
 testMarker2.bindPopup("<b>Brecksville</b><br>Cleveland Metroparks", {'autoClose': false, closeOnClick: false}).openPopup();
-
-var testMarker3 = L.marker([41.318818, -81.617775]).addTo(mymap);
-testMarker3.bindPopup("<b>Brecksville</b><br>Cleveland Metroparks", {'autoClose': false, closeOnClick: false}).openPopup();
 
 var testMarker3 = L.marker([41.222891, -81.510701]).addTo(mymap);
 testMarker3.bindPopup("<b>Virginia Kendall and Ritchie Ledges</b><br>Cuyahoga Valley National Park", {'autoClose': false, closeOnClick: false}).openPopup();
@@ -24,17 +28,15 @@ mymap.on('click', onMapClick);
 mymap.on('locationfound', onLocationFound);
 mymap.on('locationerror', onLocationError);
 
-// popups object for clicking on the map, used in onMapClick
-var popup = L.popup();
-
-// Functions:
+// Listener Functions:
 function onMapClick(e) {
+    var popup = L.popup();  // popup object for clicking on the map, used in onMapClick
     popup.setLatLng(e.latlng).setContent(e.latlng.toString()).openOn(mymap);
 }
 
 function onLocationFound(e) {
     // console.log(e);
-    var radius = e.accuracy / 3;
+    var radius = Math.round(e.accuracy / 2);
 
     L.marker(e.latlng).addTo(mymap)
         .bindPopup("You are within " + radius + " meters from this point", {'autoClose': false, closeOnClick: false})
@@ -43,6 +45,6 @@ function onLocationFound(e) {
     L.circle(e.latlng, radius).addTo(mymap);
 }
 
-function onLocationError(e) {
-    alert(e.message);
+function onLocationError(event) {
+    alert(event.message);
 }
