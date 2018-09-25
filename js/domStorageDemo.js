4fdb74be4a597e1d235106e5b0c7c510e6f068bd
Steve B <bedell.369@osu.edu>
2018-09-25 16:51:25 -0400

    CSS overhaul on the DOM Storage Demo page

--- js/domStorageDemo.js
+++ js/domStorageDemo.js
@@ -1,36 +1,37 @@
 var sessionStore = document.getElementById("sessionstore");
 var bgColorSelector = document.getElementById("bgcolor");
 
-// Loading values if present:
-if (sessionStorage.getItem("autosave")) {
-    sessionStore.value = sessionStorage.getItem("autosave");
-}
-
-if (localStorage.getItem("bgColor")) {
-    bgColorSelector.value = localStorage.getItem("bgColor");
-    changeBackgroundColor(bgColorSelector.value);
-}
-
 function changeBackgroundColor(color) {
     if (typeof color === "string") {
         document.body.style.backgroundColor = color;
     }
 }
 
+// ~~~~~ Loading values if present ~~~~~
+if (window.sessionStorage.getItem("autosave")) {
+    sessionStore.value = window.sessionStorage.getItem("autosave");
+}
+
+if (window.localStorage.getItem("bgColor")) {
+    bgColorSelector.value = window.localStorage.getItem("bgColor");
+    changeBackgroundColor(bgColorSelector.value);
+}
+
 // ~~~~~ Adding Event Listeners ~~~~~
 sessionStore.addEventListener("change", function() {
-    sessionStorage.setItem("autosave", sessionStore.value);
+    // console.log("onChange event fired, autosaving Session Storage");
+    window.sessionStorage.setItem("autosave", sessionStore.value);
 });
 
 bgColorSelector.addEventListener("change", function() {
-    var newBG = document.getElementById("bgcolor").value;
-    localStorage.setItem("bgColor", newBG);
-    changeBackgroundColor(newBG);
+    var newBgColor = document.getElementById("bgcolor").value;
+    window.localStorage.setItem("bgColor", newBgColor);
+    changeBackgroundColor(newBgColor);
 });
 
-document.getElementById("DELETE").addEventListener("click", function() {
-    if (confirm("Are you sure you want to clear storage?")) {
-        localStorage.clear();
-        sessionStorage.clear();
+document.getElementById("deleteButton").addEventListener("click", function() {
+    if (window.confirm("Are you sure you want to clear storage?")) {
+        window.localStorage.clear();
+        window.sessionStorage.clear();
     }
 });
