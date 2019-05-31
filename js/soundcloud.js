c94ddefae21cfa64afe13eb7060d14ba6dcb17a3
Steve B <bedell.369@osu.edu>
2019-05-31 15:09:08 -0400

    Updating soundcloud again...

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,11 +1,21 @@
 SC.initialize({client_id:'75579df892fafb681bb657af3fe524b6'});
 
-$(document).ready(function() {
-    SC.get('/tracks', { genres: 'dubstep' }, tracks => {
-        $(tracks).each(function(index, track) {
-            $('#results').append($('<li></li>').html(`<a target="blank" href="${track.permalink_url}">${track.title}</a>: ${track.playback_count.toLocaleString()}`));
-        });
-    });
+// $(document).ready(function() {
+//     SC.get('/tracks', { genres: 'dubstep' }, tracks => {
+//         $(tracks).each(function(index, track) {
+//             $('#results').append($('<li></li>').html(`<a target="blank" href="${track.permalink_url}">${track.title}</a>: ${track.playback_count.toLocaleString()}`));
+//         });
+//     });
+// });
+
+SC.get('/tracks', { genres: genre }, tracks => {
+	let results = "";
+	
+	tracks.forEach(track => {
+		results += `<li><img class="album-art" src="${track.artwork_url}"><a target="blank" href="${track.permalink_url}">${track.title}</a></li>`;
+	});
+	
+	document.getElementById("results").innerHTML = results;
 });
 
 function setGenre() {
@@ -14,13 +24,11 @@ function setGenre() {
 	SC.get('/tracks', { genres: genre }, tracks => {
 		console.log("tracks: ", tracks);
 		let results = "";
+		
 		tracks.forEach(track => {
-			results += `<li><img width="25px" height="25px" src="${track.artwork_url}"><a target="blank" href="${track.permalink_url}">${track.title}</a></li>`;
+			results += `<li><img class="album-art" src="${track.artwork_url}"><a target="blank" href="${track.permalink_url}">${track.title}</a></li>`;
 		});
+		
 		document.getElementById("results").innerHTML = results;
-
-		// $(tracks).each(function(index, track) {
-		// 	$('#results').append($('<li></li>').html(`<a target="blank" href="${track.permalink_url}">${track.title}</a>: ${track.playback_count.toLocaleString()}`));
-		// });
 	});
 }
