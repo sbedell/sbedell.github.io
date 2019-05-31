b39b5217b11bf0da7b181cd9ae886a24c56c38b7
Steve B <bedell.369@osu.edu>
2019-05-31 14:55:27 -0400

    Updating soundcloud page

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -12,8 +12,15 @@ function setGenre() {
 	var genre = document.getElementById('genre').value;
 	document.getElementById('results').innerHTML = '';
 	SC.get('/tracks', { genres: genre }, tracks => {
-		$(tracks).each(function(index, track) {
-			$('#results').append($('<li></li>').html(`<a target="blank" href="${track.permalink_url}">${track.title}</a>: ${track.playback_count.toLocaleString()}`));
+		console.log("tracks: ", tracks);
+		let results = "";
+		tracks.forEach(track => {
+			results += `<li><a target="blank" href="${track.permalink_url}">${track.title}</a>: ${track.playback_count.toLocaleString()}</li>`;
 		});
+		document.getElementById("results").innerHTML = results;
+
+		// $(tracks).each(function(index, track) {
+		// 	$('#results').append($('<li></li>').html(`<a target="blank" href="${track.permalink_url}">${track.title}</a>: ${track.playback_count.toLocaleString()}`));
+		// });
 	});
 }
