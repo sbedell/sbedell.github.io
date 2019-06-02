606b897a7a57412de9b5f7df34e29dc93d1551f5
Steve B <bedell.369@osu.edu>
2019-06-02 01:49:31 -0400

    Updating a few pages and styles

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,21 +1,11 @@
 SC.initialize({client_id:'75579df892fafb681bb657af3fe524b6'});
 
-// $(document).ready(function() {
-//     SC.get('/tracks', { genres: 'dubstep' }, tracks => {
-//         $(tracks).each(function(index, track) {
-//             $('#results').append($('<li></li>').html(`<a target="blank" href="${track.permalink_url}">${track.title}</a>: ${track.playback_count.toLocaleString()}`));
-//         });
-//     });
-// });
-
-SC.get('/tracks', { genres: genre }, tracks => {
-	let results = "";
-	
-	tracks.forEach(track => {
-		results += `<li><img class="album-art" src="${track.artwork_url}"><a target="blank" href="${track.permalink_url}">${track.title}</a></li>`;
-	});
-	
-	document.getElementById("results").innerHTML = results;
+$(document).ready(function() {
+    SC.get('/tracks', { genres: 'dubstep' }, tracks => {
+        $(tracks).each(function(index, track) {
+            $('#results').append($('<li></li>').html(`<img class="album-art" src="${track.artwork_url}"><a target="blank" href="${track.permalink_url}">${track.title}</a>`));
+        });
+    });
 });
 
 function setGenre() {
