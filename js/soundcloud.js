82894d29247e548377fc5f77abd6e3e008896355
Steve B <bedell.369@osu.edu>
2019-06-07 12:47:34 -0400

    Trying to get rid of jQuery on Soundcloud, can only test after pushing to github...

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,26 +1,42 @@
 SC.initialize({client_id:'75579df892fafb681bb657af3fe524b6'});
 
-$(document).ready(function() {
-    SC.get('/tracks', { genres: 'dubstep' }, tracks => {
-        $(tracks).each(function(index, track) {
-            $('#results').append($('<li></li>').html(`<img class="album-art" src="${track.artwork_url}"><a target="blank" href="${track.permalink_url}">${track.title}</a>`));
-        });
-    });
+// $(document).ready(function() {
+// 		console.log("document ready");
+//     SC.get('/tracks', { genres: 'dubstep' }, tracks => {
+//         $(tracks).each(function(index, track) {
+//             $('#results').append($('<li></li>').html(`<img class="album-art" alt="album art" src="${track.artwork_url}"><a target="blank" href="${track.permalink_url}">${track.title}</a>`));
+//         });
+//     });
+// });
+
+// Load / Search dubstep tracks on load
+document.addEventListener("DOMContentLoaded", () => {
+	getAndPopulateTracks('dubstep');
 });
 
 function searchGenre() {
 	let genre = document.getElementById('genre').value;
 
+	// reset results field
 	document.getElementById('results').innerHTML = '';
 	
-	SC.get('/tracks', { genres: genre }, tracks => {
+	if (genre && genre.match(/\w+/)) {
+		getAndPopulateTracks(genre);
+	} else {
+		document.getElementById("results").innerHTML = "Please enter a genre to search.";
+	}
+}
+
+function getAndPopulateTracks(genresToSearch) {
+	SC.get('/tracks', { genres: genresToSearch }, tracks => {
 		console.log("tracks: ", tracks);
+
 		let results = "";
 		
 		tracks.forEach(track => {
 			results += "<li>";
 			if (track.artwork_url) {
-				results += `<img class="album-art" src="${track.artwork_url}">`;
+				results += `<img class="album-art" alt="album art" src="${track.artwork_url}">`;
 			}
 			results += `<a target="blank" href="${track.permalink_url}">${track.title}</a></li>`;
 		});
