c29c0fb00f665f728137ded557e7f3a1b7dd2616
Steve B <bedell.369@osu.edu>
2019-06-21 00:29:05 -0400

    Changing back to text input...also removing XMLHTTP checks

--- js/securityScripts.js
+++ js/securityScripts.js
@@ -16,37 +16,34 @@ $(document).ready(function() {
 function ipSearch() {
     var ipAddress = document.getElementById("ipaddr").value.trim();
 	if (ipAddress.match(/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/)) {
-		if (window.XMLHttpRequest) {
-			var xmlhttp = new XMLHttpRequest();
-            toastr.info("Searching IP Address...");
-            xmlhttp.onreadystatechange = function() {
-                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                    var response = JSON.parse(xmlhttp.responseText);
-                    var output = `<p>Data for IP Address: ${response.ip.number}</p>`;
-                    if (response.ip.asname !== null) {
-                        output += `<p>Name: ${response.ip.asname}</p>`;
-                    }
-                    if (response.ip.country !== null) {
-                        output += `<p>Country: ${response.ip.country}</p>`;
-                    }
-                    if (response.ip.mindate !== null || response.ip.maxdate !== null) {
-                        output += `<p>From Dates ${response.ip.mindate} to ${response.ip.maxdate}</p>`;
-                    }
-                    if (response.ip.attacks !== null) {
-                        output += `<p>Number of Attacks (against ip): ${response.ip.attacks}</p>`;
-                    } else {
-                        output += "<p>No recorded / detected attacks against this IP address.</p>";
-                    }
-                    document.getElementById("results").innerHTML = output;
-                    toastr.clear();
+        var xmlhttp = new XMLHttpRequest();
+        toastr.info("Searching IP Address...");
+
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var response = JSON.parse(xmlhttp.responseText);
+                var output = `<p>Data for IP Address: ${response.ip.number}</p>`;
+                if (response.ip.asname !== null) {
+                    output += `<p>Name: ${response.ip.asname}</p>`;
                 }
-            };
-            var url = `https://www.dshield.org/api/ip/${ipAddress}?json`;
-            xmlhttp.open("GET", url, true);
-            xmlhttp.send();
-        } else {
-            document.getElementById("results").innerHTML = "Error, this device doesn't support XML HTTP Requests.";
-        }
+                if (response.ip.country !== null) {
+                    output += `<p>Country: ${response.ip.country}</p>`;
+                }
+                if (response.ip.mindate !== null || response.ip.maxdate !== null) {
+                    output += `<p>From Dates ${response.ip.mindate} to ${response.ip.maxdate}</p>`;
+                }
+                if (response.ip.attacks !== null) {
+                    output += `<p>Number of Attacks (against ip): ${response.ip.attacks}</p>`;
+                } else {
+                    output += "<p>No recorded / detected attacks against this IP address.</p>";
+                }
+                document.getElementById("results").innerHTML = output;
+                toastr.clear();
+            }
+        };
+        var url = `https://www.dshield.org/api/ip/${ipAddress}?json`;
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
 	} else {
 		document.getElementById("results").innerHTML = "Invalid IP (ipv4) address.";
 	}
@@ -54,34 +51,29 @@ function ipSearch() {
 
 function portSearch() {
     var port = document.getElementById("port").value.trim();
-    // Check if device supports XML Http Requests
-    if (window.XMLHttpRequest) {
-        if (port.match(/^\d+$/) && port > 0 && port < 65536) {
-            var xmlhttp = new XMLHttpRequest();
-            toastr.info("Searching Port...");
-            xmlhttp.onreadystatechange = function() {
-                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                    var response = JSON.parse(xmlhttp.responseText);
-                    var output = `<p>Data for port #: ${response.number}</p>`;
-                    if (response.services.tcp.name !== 0) {
-                        output += `<p>Port Name / Type: ${response.services.tcp.name}</p>`;
-                    }
-                    if (response.services.tcp.service !== 0) {
-                        output += `<p>TCP Service: ${response.services.tcp.service}</p>`;
-                    }
-                    output += `<p>Records: ${response.data.records}</p>`;
-                    output += `<p>Targets: ${response.data.targets}</p>`;
-                    output += `<p>Sources: ${response.data.sources}</p>`;
-                    document.getElementById("results").innerHTML = output;
-                    toastr.clear();
+    if (port.match(/^\d+$/) && port > 0 && port < 65536) {
+        var xmlhttp = new XMLHttpRequest();
+        toastr.info("Searching Port...");
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var response = JSON.parse(xmlhttp.responseText);
+                var output = `<p>Data for port #: ${response.number}</p>`;
+                if (response.services.tcp.name !== 0) {
+                    output += `<p>Port Name / Type: ${response.services.tcp.name}</p>`;
                 }
-            };
-            var url = `https://www.dshield.org/api/port/${port}?json`;
-            xmlhttp.open("GET", url, true);
-            xmlhttp.send();
-        } else {
-            document.getElementById("results").innerHTML = "Invalid port number.";
-        }
+                if (response.services.tcp.service !== 0) {
+                    output += `<p>TCP Service: ${response.services.tcp.service}</p>`;
+                }
+                output += `<p>Records: ${response.data.records}</p>`;
+                output += `<p>Targets: ${response.data.targets}</p>`;
+                output += `<p>Sources: ${response.data.sources}</p>`;
+                document.getElementById("results").innerHTML = output;
+                toastr.clear();
+            }
+        };
+        var url = `https://www.dshield.org/api/port/${port}?json`;
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
     } else {
         document.getElementById("results").innerHTML = "Error, this device doesn't support XML HTTP Requests.";
     }
