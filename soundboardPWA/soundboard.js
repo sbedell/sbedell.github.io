82214cda73598c59e3c71b7183ab081f7dded6e9
Steve B <bedell.369@osu.edu>
2019-10-14 15:09:38 -0400

    Adding soundboard.js - separate js file for Soundboard PWA

---
+++ soundboardPWA/soundboard.js
@@ -0,0 +1,26 @@
+function playAudio(audioId) {
+  if (audioId) {
+    document.getElementById(audioId).play();
+  } else {
+    console.error("Error: need to specify an audio Id to play audio");
+  }
+}
+
+function playAllAudio() {
+  let audioTracks = document.getElementsByTagName("audio");
+  for (let track of audioTracks) {
+    track.play();
+  }
+}
+
+function playRandomClips() {
+  let allAudioClips = document.getElementsByTagName("audio");
+  let len = allAudioClips.length || 0;
+
+  for (let i = 0; i < 5; i++) {
+    window.setTimeout(() => {
+      let rand = Math.floor(Math.random() * len);
+      allAudioClips[rand].play();
+    }, 350 * i);
+  }
+}
