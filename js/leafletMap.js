84a8d4978e32eebd82b89cfba2eb252736319823
Steve B <bedell.369@osu.edu>
2020-05-23 14:31:36 -0400

    Updating Leaflet map from v1.3.1 to v1.6.0

--- js/leafletMap.js
+++ js/leafletMap.js
@@ -14,11 +14,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Addings some markers to the map, some with popups 
 let clevelandMarker = L.marker([41.49985, -81.6938]).addTo(mymap);
 
-let testMarker = L.marker([41.219857, -81.698456]).addTo(mymap);
-testMarker.bindPopup("<b>Whipps Ledges</b><br>Cleveland Metroparks", {'autoClose': false, closeOnClick: false}).openPopup();
+// let testMarker = L.marker([41.219857, -81.698456]).addTo(mymap);
+// testMarker.bindPopup("<b>Whipps Ledges</b><br>Cleveland Metroparks", {'autoClose': false, closeOnClick: false}).openPopup();
 
-let testMarker2 = L.marker([41.318818, -81.617775]).addTo(mymap);
-testMarker2.bindPopup("<b>Brecksville</b><br>Cleveland Metroparks", {'autoClose': false, closeOnClick: false}).openPopup();
+// let testMarker2 = L.marker([41.318818, -81.617775]).addTo(mymap);
+// testMarker2.bindPopup("<b>Brecksville</b><br>Cleveland Metroparks", {'autoClose': false, closeOnClick: false}).openPopup();
 
 let testMarker3 = L.marker([41.222891, -81.510701]).addTo(mymap);
 testMarker3.bindPopup("<b>Virginia Kendall and Ritchie Ledges</b><br>Cuyahoga Valley National Park", {'autoClose': false, closeOnClick: false}).openPopup();
