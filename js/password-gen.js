14976ee2c6bb08e65acf9348499402fdcbe12ef0
Steve B <bedell.369@osu.edu>
2021-06-05 14:13:08 -0400

    Updating password-generator.html to use regualar functions instead of modifying String prototype.

--- js/password-gen.js
+++ js/password-gen.js
@@ -5,38 +5,38 @@
  * for the original skeleton of this code
  */
 
-// using default parameters for max = min
-String.prototype.pick = function(min, max = min) {
-  let n = min + Math.floor(Math.random() * (max - min));
-  let chars = '';
+function pickCharactersFromString(inputStr, numChars = 0) {
+  let chars = "";
 
-  for (let i = 0; i < n; i++) {
-    chars += this.charAt(Math.floor(Math.random() * this.length));
+  for (let i = 0; i < numChars; i++) {
+    chars += inputStr.charAt(Math.floor(Math.random() * inputStr.length));
   }
 
   return chars;
-};
+}
 
 /**
 * Shuffle a string, basically implementation of Fisher–Yates shuffle
 * Credit to @Christoph: https://stackoverflow.com/a/962890/464744
 */
-String.prototype.shuffle = function() {
-  let splitString = this.split('');
-  let top = splitString.length;
-  let tmp, current;
+function shuffleString(inputStr) {
+  if (inputStr) {
+    let splitString = inputStr.split("");
+    let top = splitString.length;
+    let tmp, current;
 
-  if (top) {
     while (--top) {
       current = Math.floor(Math.random() * (top + 1));
       tmp = splitString[current];
       splitString[current] = splitString[top];
       splitString[top] = tmp;
     }
-  }
 
-  return splitString.join('');
-};
+    return splitString.join("");
+  } else {
+    return "";
+  }
+}
 
 function generateRandomPassword(options) {
   let lowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -52,32 +52,27 @@ function generateRandomPassword(options) {
   let password = "";
   let totalOptions = 0;
 
-  // for (opt in options) {
-  //   console.log(`${opt}: ${options[opt]}`);
-  // }
-
-  // Add lowercase to set of all characters, and pick at least 1 for the pw
   if (options.useLower) {
     allChars += lowercase;
-    password += lowercase.pick(1);
+    password += pickCharactersFromString(lowercase, 1);
     totalOptions++;
   }
 
   if (options.useUpper) {
     allChars += uppercase;
-    password += uppercase.pick(1);
+    password += pickCharactersFromString(uppercase, 1);
     totalOptions++;
   }
 
   if (options.useNumbers) {
     allChars += numbers;
-    password += numbers.pick(1);
+    password += pickCharactersFromString(numbers, 1);
     totalOptions++;
   }
 
   if (options.useSpecialChars) {
     allChars += specialChars;
-    password += specialChars.pick(1);
+    password += pickCharactersFromString(specialChars, 1);
     totalOptions++;
   }
 
@@ -90,17 +85,25 @@ function generateRandomPassword(options) {
   //    console.log("Uppercase: ", uppercase);
   // }
 
-  // console.log(options);
-  let len = (options.passLength) ? options.passLength : 8;
-  //let password = (lowercase.pick(1) + uppercase.pick(1) + numbers.pick(1) + specialChars.pick(1) + all.pick(len - 4)).shuffle();
-  password += allChars.pick(len - totalOptions);
-  password = password.shuffle();
-
-  return password;
+  // console.log("options: ", options);
+  
+  let len = 12;
+  if (!options.passLength || options.passLength > 99 || options.passLength < 12) {
+    // console.error("Warning: Password length under 12 is insecure and over 99 is usually not widely accepted by all systems. Defaulting to length 12.");
+    document.getElementById("error-output").innerText = "Warning: Password length under 12 is insecure and over 99 is usually not widely accepted by all systems. Defaulting to length 12.";
+  } else {
+    len = options.passLength;
+    document.getElementById("error-output").innerText = "";
+  }
+  
+  password += pickCharactersFromString(allChars, len - totalOptions);
+  // console.log("password pre shuffle: ", password);
+  
+  return shuffleString(password);
 }
 
 function passwordGen() {
-  let passLen = parseInt(document.getElementById("pass-length").value);
+  let passLen = parseInt(document.getElementById("pw-length").value);
   let useUppercase = document.getElementById("uppercaseCb").checked;
   let useLowercase = document.getElementById("lowercaseCb").checked;
   let useNums = document.getElementById("numbersCb").checked;
